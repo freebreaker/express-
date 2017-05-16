0438651fe7554b5d51e95a73bccbf55b5d495c30
freebreaker <592378845@qq.com>
2017-05-17 00:20:57 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+// var express = require('express');
+// var router = express.Router();
+
+// // router.get('/', function(req, res) {
+// //   res.send('hello, express');
+// // });
+
+// // module.exports = router;
+
+
+// module.exports = function (app) {
+//   app.get('/', function (req, res) {
+//     res.redirect('/posts');
+//   });
+//   app.use('/signup', require('./signup'));
+//   app.use('/signin', require('./signin'));
+//   app.use('/signout', require('./signout'));
+//   app.use('/posts', require('./posts'));
+// };
+
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts');
+  });
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));  
+  // 404 page
+  app.use(function (req, res) {
+    if (!res.headersSent) {
+      res.render('404');
+    }
+  });
+};
